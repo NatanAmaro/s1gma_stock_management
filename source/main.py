826c5cd0816b5cc102b140492a193afb730ffacb
NatanAmaro <47829269+NatanAmaro@users.noreply.github.com>
2024-07-06 01:27:13 -0300

    save state.

--- source/main.py
+++ source/main.py
@@ -1,14 +1,35 @@
 from constants import alphaKey
+from pandasql import sqldf
 from io import StringIO
 import pandas as pd
 import requests
 
 
-def main(stocks):
-    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=' \
-          f'{stocks}&apikey={alphaKey}&datatype=csv'
-    r = requests.get(url)
+class StocksApi:
 
-    df = pd.read_csv(StringIO(r.text))
+    @staticmethod
+    def runApi(stocks):
+        """
 
-    return df
+        :param stocks:
+        :return:
+        """
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=' \
+              f'{stocks}&apikey={alphaKey}&datatype=csv'
+        r = requests.get(url)
+
+        df = pd.read_csv(StringIO(r.text))
+
+        return df
+
+    def selectWallets(self, stocksList):
+        """
+
+        :param stocksList:
+        :return:
+        """
+
+        for stocks in stocksList:
+            data =
+
+        return recomendation
